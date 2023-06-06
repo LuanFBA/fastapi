661549739ce03beb20e9e5b5a8abee8a644237cc
LuanFBA <luanfba.araujo@hotmail.com>
2023-06-06 11:06:02 -0300

    build: adicionando arquivo main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.api_v1.api import api_router
+from app.api.router import api_router
 
 
 app = FastAPI()
@@ -16,4 +16,4 @@ app.add_middleware(CORSMiddleware,
 
 
 # ROTAS
-app.include_router(api_router, prefix="/api/v1")
+app.include_router(router=api_router, prefix="/api/v1")
