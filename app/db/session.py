2fe63c2efe9639c77502da82a9a09e3b4c060750
LuanFBA <luanfba.araujo@hotmail.com>
2023-06-06 11:17:20 -0300

    build: Adicionando arquivo session

--- app/db/session.py
+++ app/db/session.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite_db.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://fastapi:123456@localhost/fastapi_db"
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite_db.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://teste:teste@luan-db/luan-db"
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASS')}@{config('DB_HOST')}/{config('DB_BASE')}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
