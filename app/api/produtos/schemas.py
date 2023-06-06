1c559981805677b248bd3cc758c1df7594910075
LuanFBA <luanfba.araujo@hotmail.com>
2023-06-06 11:21:11 -0300

    Refactor: Organizando arquivos

---
+++ app/api/produtos/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class ProdutoBase(BaseModel):
+    nome: str
+    detalhes: Optional[str]
+    preco: Optional[float]
+
+
+class ProdutoCreate(ProdutoBase):
+    usuario_id: int
+
+
+class Produto(ProdutoBase):
+    id: Optional[int] = None
+    disponivel: bool
+    usuario_id: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+
+class ProdutoSimples(ProdutoBase):
+    id: Optional[int]
+    nome: str
+    detalhes: Optional[str]
+    preco: Optional[float]
+    disponivel: bool
+    usuario_id: Optional[int]
+
+    class Config:
+        orm_mode = True
