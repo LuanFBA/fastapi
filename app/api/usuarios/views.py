aed6d76fa5317ecd3a191fa84a93b2fb595588eb
LuanFBA <luanfba.araujo@hotmail.com>
2023-06-07 11:23:00 -0300

    refactor: .

--- app/api/usuarios/views.py
+++ app/api/usuarios/views.py
@@ -21,14 +21,18 @@ def listar_usuarios(db: Session = Depends(get_db)):
 
 @usuarios_router.get("/me")
 def obter_usuario_me(usuario: Usuario = Depends(obter_usuario_logado)):
+
+    if usuario.ativo == False:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado!")
+
     return usuario
 
 
-@usuarios_router.delete("/")
-def remover_usuario(db: Session = Depends(get_db), usuario: Usuario = Depends(obter_usuario_logado)):
-    usuario_removido = RepositorioUsuario(db).remover(id=usuario.id)
+# @usuarios_router.delete("/")
+# def remover_usuario(db: Session = Depends(get_db), usuario: Usuario = Depends(obter_usuario_logado)):
+#     usuario_removido = RepositorioUsuario(db).remover(id=usuario.id)
 
-    return usuario_removido
+#     return usuario_removido
 
 
 @usuarios_router.get("/{usuario_id}", response_model=Usuario)
